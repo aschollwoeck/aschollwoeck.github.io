e83857bf12015d271fe1d13aca03bcce3c209c0f
QualmandDriven <a.schollwoeck@outlook.com>
2017-03-19 22:00:11 +0100

    Update

---
+++ hugo/build.bat
@@ -0,0 +1,10 @@
+echo WebSite mit Hugo erstellen...
+hugo.exe
+
+echo Ordner kopieren
+xcopy /y /s ".\public\*" "..\"
+
+echo Zu Github pushen
+git add --all
+git commit -m "Update"
+git push origin master
